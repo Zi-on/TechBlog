e0765eb81ac2986b51c3f0550d0d89aa5e78e569
Zi-on <zioniflores12@yahoo.com>
2021-08-07 23:34:04 -0500

    fixed my routes to render homepage and login page

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const homeRoutes = require('./userRoutes');
+const userRoutes = require('./userRoutes');
 
-router.use('/', homeRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,36 +1,48 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers/api');
-const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const path = require("path");
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
+const exphbs = require("express-handlebars");
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+//const hbs = exphbs.create("view engine", "handlebars");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+app.use(
+  session({
+    secret: "Super secret secret",
+    cookie: {
+      maxAge: 1800000,
+    },
+    rolling: true,
+    resave: true,
     saveUninitialized: true,
     store: new SequelizeStore({
-      db: sequelize
-    })
-  };
-  const hbs = exphbs.create({ helpers });
+      db: sequelize,
+    }),
+  })
+);
+ 
 
-  app.engine('handlebars', hbs.engine);
-  app.set('view engine', 'handlebars');
-  
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, 'public')));
-  
-  app.use(routes);
+
+// app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now Listening localhost:3001"));
 });
