6f3707f1af5d69900ba2fcc15b6b78a90b170854
Zi-on <zioniflores12@yahoo.com>
2021-08-10 00:10:30 -0500

    added login, logout and signup javascript

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,41 +2,42 @@ const router = require('express').Router();
 const { User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
-    const users = userData.map((project) => project.get({ plain: true }));
-    res.render('homepage', {
-      users,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll({
+            attributes: { exclude: 'password'},
+        });
+        res.status(200).json(userData)
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
-router.get('/login', (req, res) => {
-  if (req.session) {
-    res.redirect('/');
-    return;
-  }
-  res.render('login');
-});``
+
+router.get('/:id', async (req, res) => {
+    try {
+        const userId = await User.findByPk(req.params.id);
+        res.status(200).json(userId)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 router.post('/', async (req, res) => {
     try {
-      const userData = await User.create(req.body);
+      const dbUserData = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
   
       req.session.save(() => {
-        req.session.user_id = userData.id;
         req.session.logged_in = true;
   
-        res.status(200).json(userData);
+        res.status(200).json(dbUserData);
       });
     } catch (err) {
-      res.status(400).json(err);
+      console.log(err);
+      res.status(500).json(err);
     }
   });
 
@@ -72,12 +73,31 @@ router.post('/', async (req, res) => {
     }
   });
   
+  router.delete('/:id', async (req, res) => {
+      try {
+          const userData = await User.destroy({ 
+              where: { 
+                  id: req.params.id
+              }
+          });
+
+          if (!userData) {
+              res.status(404).json({ message: "No user found with that id"});
+              return;
+          }
+          res.status(200).json(userData);
+      } catch (err) {
+          res.status(500).json(err);
+      }
+  })
+
   router.post('/logout', (req, res) => {
     if (req.session.logged_in) {
       req.session.destroy(() => {
         res.status(204).end();
       });
     } else {
+        console.log("hiß")
       res.status(404).end();
     }
   });
