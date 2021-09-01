9c6633376c3ed41c66b401da5dffc429c76b2e37
Zi-on <zioniflores12@yahoo.com>
2021-08-31 21:24:50 -0500

    working on getting profile to only populate with its own posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,19 +4,14 @@ const withAuth = require('../utils/auth');
 const { post } = require('./api');
 
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
     try {
       // Get all projects and JOIN with user data
-      const postData = await Post.findAll({
-          attributes: [
-            'id',
-            'title',
-            'body'
-          ],
+      const userData = await User.findByPk(req.params.id, {
         include: [
           {
-            model: User,
-            attributes: ['name'],
+            model: Post,
+            attributes: ['id', 'title', 'body', 'user_id'],
           },
           {
             model: Comment,
@@ -30,11 +25,11 @@ router.get('/', async (req, res) => {
       });
   
       // Serialize data so the template can read it
-      const posts = postData.map(post => post.get({ plain: true }));
+      const user = userData.get({ plain: true });
   
       // Pass serialized data and session flag into template
       res.render('homepage', { 
-        posts, 
+        user, 
         logged_in: req.session.logged_in 
       });
     } catch (err) {
@@ -44,7 +39,8 @@ router.get('/', async (req, res) => {
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/dashboard');
+    const id = req.params.id;
+    res.redirect(`/profile/${id}`);
     return;
   }
   res.render('login', {
@@ -54,7 +50,8 @@ router.get('/login', (req, res) => {
 
 router.get('/signup', (req, res) => {
     if (req.session.logged_in) {
-      res.redirect('/');
+      const id = req.params.id
+      res.redirect(`profile/${id}`);
       return;
     }
     res.render('signup', {
@@ -100,45 +97,62 @@ router.get('/signup', (req, res) => {
     }
   });
 
+  router.get('/profile/:id', withAuth, async (req, res) => {
+    try {
+      const userData = await User.findByPk(req.params.id, {
+        include: [{
+          model: Post,
+          attributes: ['id', 'title', 'body'],
+        }],
+      });
+      const user = userData.get({ plain: true});
+      res.render('profile', { user, logged_in: req.session.logged_in });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err)
+    }
+  })
  
       // Get all projects and JOIN with user data
-      router.get('/profile', async (req, res) => {
-        try {
-          // Get all projects and JOIN with user data
-          const postData = await Post.findAll({
-              attributes: [
-                'id',
-                'title',
-                'body'
-              ],
-            include: [
-              {
-                model: User,
-                attributes: ['name'],
-              },
-              {
-                model: Comment,
-                attributes: ['id', 'text', 'user_id', 'post_id'],
-                include: {
-                    model: User,
-                    attributes: ['name']
-                }
-              }
-            ],
-          });
+      // router.get('/profile', async (req, res) => {
+      //   try {
+      //     // Get all projects and JOIN with user data
+      //     const postData = await Post.findAll({
+      //         attributes: [
+      //           'id',
+      //           'title',
+      //           'body'
+      //         ],
+
+      //       include: [
+      //         {
+      //           model: User,
+      //           attributes: ['name'],
+      //         },
+      //         {
+      //           model: Comment,
+      //           attributes: ['id', 'text', 'user_id', 'post_id'],
+      //           include: {
+      //               model: User,
+      //               attributes: ['name']
+      //           }
+      //         }
+      //       ],
+      //     });
       
-          // Serialize data so the template can read it
-          const posts = postData.map(post => post.get({ plain: true }));
       
-          // Pass serialized data and session flag into template
-          res.render('profile', { 
-            posts, 
-            logged_in: req.session.logged_in 
-          });
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      });
+      //     // Serialize data so the template can read it
+      //     const posts = postData.map(post => post.get({ plain: true }));
+      
+      //     // Pass serialized data and session flag into template
+      //     res.render('profile', { 
+      //       posts, 
+      //       logged_in: req.session.logged_in 
+      //     });
+      //   } catch (err) {
+      //     res.status(500).json(err);
+      //   }
+      // });
 
            // Get all projects and JOIN with user data
            router.get('/addPost', async (req, res) => {
